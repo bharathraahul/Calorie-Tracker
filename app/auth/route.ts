56b1caddc196ae2eb50ce954a4b55f662a0e855b
bharathraahul <raahulbharath@gmail.com>
2026-04-14 10:57:55 -0500

    refactor: Fix user registration and password hashing

--- app/auth/route.ts
+++ app/auth/route.ts
@@ -1,19 +1,15 @@
 import { prisma } from "@/prisma/prisma";
 import bcrypt from "bcryptjs";
 
-export async function saveUser(request: Request){
+export async function POST(request: Request){
 
     const {name,email,password} = await request.json()
 
-    if (request.method == 'POST'){
-
-        if (!await prisma.user.findUnique({email})){
-            await bcrypt.hash(password,10)
-            await prisma.user.create({data: {name,email,password}})
+    if (!await prisma.user.findUnique({where: {email}})){
+            const hashedpassword = await bcrypt.hash(password,10)
+            await prisma.user.create({data: {name,email,password:hashedpassword}})
 
         }
-
-    }
 
     return Response.json({message:"User created successfully"},{status:201})
 
